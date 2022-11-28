61d182a756da7596486eeb95dca9d6f406139896
jjliggett <67353173+jjliggett@users.noreply.github.com>
2022-11-27 22:08:25 -0500

    feat: Add initial .NET MAUI app with Android binary (#79)

---
+++ CurrentTimeApp.MauiBlazor/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace CurrentTimeApp.MauiBlazor;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        if (window is not null)
+        {
+            window.Title = "Current Time App";
+        }
+
+        return window;
+    }
+}
---
+++ CurrentTimeApp.MauiBlazor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace CurrentTimeApp.MauiBlazor;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
